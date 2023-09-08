aa527e76e3bb246d96079af233a79b0ae3d16f25
falco <it21223594@my.sliit.lk>
2023-09-08 07:56:21 +0530

    Added SMS generation feature- Tested and working

--- app/src/main/java/com/example/eyecare/activities/ActivateEmergencyContactActivity.kt
+++ app/src/main/java/com/example/eyecare/activities/ActivateEmergencyContactActivity.kt
@@ -1,13 +1,17 @@
 package com.example.eyecare.activities
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.pdf.PdfDocument
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.telephony.SmsManager
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.eyecare.R
 import com.example.eyecare.database.EyecareDatabase
 import com.example.eyecare.database.entities.EmergencyDetails
@@ -23,6 +27,14 @@ import java.util.Date
 import java.util.Locale
 
 class ActivateEmergencyContactActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 123
+
+    lateinit var cMsg :String
+    lateinit var cNo1 :String
+    lateinit var cNo2 :String
+    lateinit var cNo3 :String
+    lateinit var cName :String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activate_emergency_contact)
@@ -38,27 +50,58 @@ class ActivateEmergencyContactActivity : AppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.IO) {
             fetchDetails()
+
+            if (ContextCompat.checkSelfPermission(this@ActivateEmergencyContactActivity, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@ActivateEmergencyContactActivity, arrayOf(android.Manifest.permission.SEND_SMS), PERMISSION_REQUEST_CODE)
+            } else {
+                // Permission already granted, proceed to send SMS
+                sendSMS(cNo1, cNo2,cNo3,cMsg, cName)
+            }
         }
 
 
     }
 
+    private fun sendSMS(no1:String, no2:String, no3:String,msg:String, name:String) {
+        val phoneNumber1 = no1
+        val phoneNumber2 = no2
+        val phoneNumber3 = no3
+        val message = "This message is generated from EyeCare app. A Patient named ${name} is in emergency. Here is their message:- ${msg}"
+
+        try {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber1, null, message, null, null)
+            smsManager.sendTextMessage(phoneNumber2, null, message, null, null)
+            smsManager.sendTextMessage(phoneNumber3, null, message, null, null)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
     suspend fun fetchDetails(){
 
-        var currentContact1 = findViewById<TextView>(R.id.activatedNumber1)
-        var currentContact2 = findViewById<TextView>(R.id.activatedNumber2)
-        var currentContact3 = findViewById<TextView>(R.id.activatedNumber3)
-        var currentName = findViewById<TextView>(R.id.activatedNameTxt)
+
 
 
         coroutineScope {
             val dataList: List<EmergencyDetails> = getEmergencyDetails()
             launch(Dispatchers.Main) {
                 // Update UI on the main thread
+                var currentContact1 = findViewById<TextView>(R.id.activatedNumber1)
+                var currentContact2 = findViewById<TextView>(R.id.activatedNumber2)
+                var currentContact3 = findViewById<TextView>(R.id.activatedNumber3)
+                var currentName = findViewById<TextView>(R.id.activatedNameTxt)
+
                 currentName.text =dataList.getOrNull(0)?.uName ?: "Name not found"
                 currentContact1.text = (dataList.getOrNull(0)?.contNo1 ?: "Not available" )
                 currentContact2.text = (dataList.getOrNull(0)?.contNo2 ?: "Not available")
                 currentContact3.text = (dataList.getOrNull(0)?.contNo3 ?: "Not available")
+
+                cMsg = dataList.getOrNull(0)?.emergMsg?:"Not available"
+                cNo1 = currentContact1.text.toString()
+                cNo2 = currentContact2.text.toString()
+                cNo3 = currentContact3.text.toString()
+                cName=currentName.text.toString()
 
                 createPdf()
 
